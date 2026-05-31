30e887d6d85864bb9a604fe619c9582307fc8a59
DESKTOP\healt <healthnet@naver.com>
2026-05-31 20:37:44 +0900

    add: lambda 호출 추가

--- service/routes/auth.js
+++ service/routes/auth.js
@@ -4,11 +4,20 @@ const {
   postLogin,
   getUsers
 } = require("../controllers/authController");
+const { proxyOrFallback } = require("../utils/gatewayProxy");
 
 const router = express.Router();
 
-router.post("/register", postRegister);
-router.post("/login", postLogin);
-router.get("/users", getUsers);
+router.post("/register", (req, res) =>
+  proxyOrFallback(req, res, "/auth/register", postRegister)
+);
+
+router.post("/login", (req, res) =>
+  proxyOrFallback(req, res, "/auth/login", postLogin)
+);
+
+router.get("/users", (req, res) =>
+  proxyOrFallback(req, res, "/auth/users", getUsers)
+);
 
 module.exports = router;
